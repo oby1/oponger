1a59dcc947516237d3154bf3ebb59c566542124e
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2011-03-14 23:16:43 -0400

    List players. "dumb" player signup, which doesn't include first searching for a player.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
 import os
 from google.appengine.ext.webapp import template
 
@@ -8,10 +9,12 @@ from google.appengine.ext.webapp import template
 class MainPage(webapp.RequestHandler):
   def get(self):
     user = users.get_current_user()
+    players = db.GqlQuery("SELECT * FROM Player ORDER BY date DESC")
 
     if user:
       template_values = {
         'nickname': user.nickname(),
+        'players' : players,
       }
       path = os.path.join(os.path.dirname(__file__), 'index.html')
       self.response.out.write(template.render(path, template_values))
@@ -19,8 +22,22 @@ class MainPage(webapp.RequestHandler):
     else:
       self.redirect(users.create_login_url(self.request.uri))
 
+class SignUp(webapp.RequestHandler):
+  def post(self):
+    player = Player(pseudonym=self.request.get('pseudonym'))
+    if users.get_current_user():
+      player.user = users.get_current_user()
+    player.put()
+    self.redirect('/')
+
+class Player(db.Model):
+  user = db.UserProperty()
+  pseudonym = db.StringProperty(multiline=True, required=True)
+  date = db.DateTimeProperty(auto_now_add=True)
+
 application = webapp.WSGIApplication(
-  [('/', MainPage)],
+  [('/', MainPage),
+  ('/signup', SignUp)],
   debug=True)
 
 def main():
