1cffeb3ed03c66dc4d52cce7f345fba8b04bf513
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2011-03-18 21:28:03 -0400

    More strict inequality.

--- lib/rules.py
+++ lib/rules.py
@@ -26,7 +26,7 @@ but got %s and %s points for players.""" % (POINTS_TO_WIN, player_1_score, playe
 
 def validate_score_diff(player_1_score, player_2_score):
   if (player_1_score > POINTS_TO_WIN - 1 or player_2_score > POINTS_TO_WIN - 1)\
-      and math.fabs(player_1_score - player_2_score) < POINT_DIFF_FOR_WIN:
+      and math.fabs(player_1_score - player_2_score) != POINT_DIFF_FOR_WIN:
     raise Exception("Invalid win difference, got %s and %s points for players."
     % (player_1_score, player_2_score))
 
