8958b1b20f3353b397451a1ef479eb545baa9a79
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2011-03-20 21:49:20 -0400

    Fix divide-by-zero error.

--- lib/stats.py
+++ lib/stats.py
@@ -7,8 +7,11 @@ def stats(player):
   for game in games:
     if game.winner().key() == player.key():
       wins += 1
+
+  num_games = len(games)
+  percent_win = 0 if num_games == 0 else int(100 * float(wins) / len(games))
   return {
     'losses': len(games) - wins,
     'wins': wins,
-    'percent_win': int(100 * float(wins) / len(games))
+    'percent_win': percent_win
   }
