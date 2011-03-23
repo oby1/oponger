3b82eb4f48af1b8e9d2012f9240a97758d169992
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2011-03-23 13:27:41 -0400

    Update default gravatar image.

--- templatetags/gravatar.py
+++ templatetags/gravatar.py
@@ -9,7 +9,8 @@ from django.utils import simplejson
 
 GRAVATAR_URL_PREFIX = getattr(settings, "GRAVATAR_URL_PREFIX",
                                       "http://www.gravatar.com/")
-GRAVATAR_DEFAULT_IMAGE = "http://www.gravatar.com/avatar/ee35bb0a4baef8f2b8fb395289999a88?s=80&amp;r=g"
+
+GRAVATAR_DEFAULT_IMAGE = "http://i.imgur.com/GADRJ.png"
 GRAVATAR_DEFAULT_RATING = getattr(settings, "GRAVATAR_DEFAULT_RATING", "g")
 GRAVATAR_DEFAULT_SIZE = getattr(settings, "GRAVATAR_DEFAULT_SIZE", 80)
 GRAVATAR_IMG_CLASS = getattr(settings, "GRAVATAR_IMG_CLASS", "gravatar")
